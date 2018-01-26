4eb36d48e9b6339deafa909eb7d468dca13ea588
Ido Feins <ifeins@gmail.com>
2018-01-26 18:12:04 +0200

    Fixed ProGuard configuration.

--- app/src/main/java/com/ifeins/tenbis/activities/SignUpActivity.java
+++ app/src/main/java/com/ifeins/tenbis/activities/SignUpActivity.java
@@ -1,17 +1,22 @@
 package com.ifeins.tenbis.activities;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.ifeins.tenbis.R;
 import com.ifeins.tenbis.models.User;
 import com.ifeins.tenbis.services.TenbisMonitorService;
+
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -29,9 +34,16 @@ public class SignUpActivity extends AppCompatActivity {
 
         mEmailView = findViewById(R.id.input_email);
         mPasswordView = findViewById(R.id.input_password);
+        mPasswordView.setOnEditorActionListener((textView, i, keyEvent) -> {
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                onSignUp(null);
+                return true;
+            }
+            return false;
+        });
     }
 
-    public void onSignUp(View view) {
+    public void onSignUp(@Nullable View view) {
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
@@ -55,19 +67,24 @@ public class SignUpActivity extends AppCompatActivity {
                     User.setCurrentUser(user);
                     startActivity(new Intent(SignUpActivity.this, OverallActivity.class));
                 } else {
-                    showError();
+                    try {
+                        showError(response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        showError(e.getMessage());
+                    }
                 }
             }
 
             @Override
             public void onFailure(Call<Void> call, Throwable t) {
-                showError();
+                showError(t.getMessage());
             }
         });
     }
 
-    private void showError() {
-        // TODO: display error
+    private void showError(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
     }
 }
 
